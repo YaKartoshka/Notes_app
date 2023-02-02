5c6152342364eede48bf08b0781ec58843ae3868
YaKartoshka <100137723+YaKartoshka@users.noreply.github.com>
2023-02-03 04:11:18 +0600

    commit

--- app/src/main/java/com/example/notesapp/AddNote.kt
+++ app/src/main/java/com/example/notesapp/AddNote.kt
@@ -1,10 +1,20 @@
 package com.example.notesapp
 
 import android.app.Activity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.getSystemService
 import com.example.notesapp.Models.Note
 import com.example.notesapp.databinding.ActivityAddNoteBinding
 import com.example.notesapp.databinding.ActivityMainBinding
@@ -18,10 +28,12 @@ class AddNote : AppCompatActivity() {
 
     private lateinit var note: Note
     private lateinit var old_note: Note
+    private val CHANNEL_ID = "channel_id_example_01"
+    private val notificationId=101
     var isUpdate = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        createNotificationChannel()
         binding = ActivityAddNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -54,7 +66,7 @@ class AddNote : AppCompatActivity() {
                         null, title, note_desc, formatter.format(Date())
                     )
                 }
-
+                sendNotification(title)
                 val intent = Intent()
                 intent.putExtra("note", note)
                 setResult(Activity.RESULT_OK, intent)
@@ -68,6 +80,40 @@ class AddNote : AppCompatActivity() {
 
         binding.imgBackArrow.setOnClickListener {
             onBackPressed()
+        }
+    }
+
+    private fun createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val name = "Notification Title"
+            val descriptionText = "Notification Description"
+            val importance: Int = NotificationManager.IMPORTANCE_DEFAULT
+            val channel: NotificationChannel = NotificationChannel(CHANNEL_ID,name,importance).apply {
+                description=descriptionText
+            }
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun sendNotification(title:String){
+        val intent = Intent(this,MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this,0,intent,0)
+        val bitmap = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.ic_launcher_background)
+        val bitmapLargeIcon = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.ic_launcher_background)
+        val builder = NotificationCompat.Builder(this,CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("Notes app")
+            .setContentText("Your note ${title} was added successfully!")
+            .setLargeIcon(bitmapLargeIcon)
+            .setStyle(NotificationCompat.BigPictureStyle().bigPicture(bitmap))
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)){
+            notify(notificationId, builder.build())
         }
     }
 }
